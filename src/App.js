75ca572a0795941a1be09da6ecf3bc921f7af580
Volkova Annie <annie_v@bk.ru>
2018-09-01 19:21:04 +0300

    Add components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import DragAndDrop from './components/DragAndDrop';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DragAndDrop />
+      </div>
+    );
+  }
+}
+
+export default App;
