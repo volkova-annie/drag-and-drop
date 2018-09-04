2f04666ff9eb9aef30d692f988f232b17ab9cbc5
Volkova Annie <annie_v@bk.ru>
2018-09-04 13:52:12 +0300

    Fix formatting

--- src/components/DragAndDrop/index.js
+++ src/components/DragAndDrop/index.js
@@ -13,168 +13,168 @@ class DragAndDrop extends Component {
     this.processJson = this.processJson.bind(this);
 
     this.state = {
-    	result: [],
-	    errors: [],
+      result: [],
+      errors: [],
     }
   };
 
   componentDidMount() {
-	  document.addEventListener('dragover', function(event) {
-		  event.preventDefault();
-	  });
-	  document.addEventListener('drop', function(event) {
-		  event.preventDefault();
-	  });
+    document.addEventListener('dragover', function(event) {
+      event.preventDefault();
+    });
+    document.addEventListener('drop', function(event) {
+      event.preventDefault();
+    });
   }
 
-	setDraggableClass(component, isDraggable) {
-		if (isDraggable) {
-			component.classList.add(styles.dragging);
-		} else {
-			component.classList.remove(styles.dragging);
-		}
-	}
+  setDraggableClass(component, isDraggable) {
+    if (isDraggable) {
+      component.classList.add(styles.dragging);
+    } else {
+      component.classList.remove(styles.dragging);
+    }
+  }
 
-	isJsonValid(json) {
-		try {
-			return JSON.parse(json);
-		}
-		catch (error) {
-			this.setState({errors: [...this.state.errors, error.toString()]});
-		}
-		return false;
-	}
+  isJsonValid(json) {
+    try {
+      return JSON.parse(json);
+    }
+    catch (error) {
+      this.setState({errors: [...this.state.errors, error.toString()]});
+    }
+    return false;
+  }
 
-	countObjects(string) {
-	  const openCurlyBrace = '{';
-	  const closeCurlyBrace = '}';
-	  let counterObjects = 0;
-		let numOpenBraces = 0;
+  countObjects(string) {
+    const openCurlyBrace = '{';
+    const closeCurlyBrace = '}';
+    let counterObjects = 0;
+    let numOpenBraces = 0;
 
-	  for (let i = 0; i < string.length; i++) {
-	    const element = string[i];
-	    if (element === openCurlyBrace) {
-		    numOpenBraces++;
+    for (let i = 0; i < string.length; i++) {
+      const element = string[i];
+      if (element === openCurlyBrace) {
+        numOpenBraces++;
       }
       else if (element === closeCurlyBrace) {
-	      if (--numOpenBraces < 0) {
-		      this.setState({errors: [...this.state.errors, 'Wrong json']});
-	        return;
+        if (--numOpenBraces < 0) {
+          this.setState({errors: [...this.state.errors, 'Wrong json']});
+          return;
         }
-	      counterObjects++;
+        counterObjects++;
       }
     }
-		if (numOpenBraces > 0) {
-			this.setState({errors: [...this.state.errors, 'Wrong json']});
-		}
-		return counterObjects;
+    if (numOpenBraces > 0) {
+      this.setState({errors: [...this.state.errors, 'Wrong json']});
+    }
+    return counterObjects;
   }
 
-	processJson(data, name) {
-	  if (this.isJsonValid(data)) {
-	    const countObjects = this.countObjects(data);
-		  this.setState({result: [...this.state.result, `Number of objects in ${name}: ${countObjects}`]});
+  processJson(data, name) {
+    if (this.isJsonValid(data)) {
+      const countObjects = this.countObjects(data);
+      this.setState({result: [...this.state.result, `Number of objects in ${name}: ${countObjects}`]});
     } else {
-		  this.setState({ errors: [...this.state.errors, 'Invalid format']});
+      this.setState({ errors: [...this.state.errors, 'Invalid format']});
     }
   }
 
-	readFiles(files) {
-		let reader = new FileReader();
-		const filesArray = [...files];
-		const allowedTypes = ['application/json'];
-		let fileName = '';
+  readFiles(files) {
+    let reader = new FileReader();
+    const filesArray = [...files];
+    const allowedTypes = ['application/json'];
+    let fileName = '';
 
-		let errors = [];
+    let errors = [];
 
-		const readNext = () => {
-			const target = filesArray.shift();
-			if (target) {
-				if (allowedTypes.indexOf(target.type) === -1) {
-					errors.push(`Unsupported file type: ${target.name}`);
-					readNext();
-					return;
-				}
-				fileName = target.name;
-				reader.readAsBinaryString(target);
-			} else {
-				this.setState({errors: [...this.state.errors, ...errors]});
-			}
-		};
+    const readNext = () => {
+      const target = filesArray.shift();
+      if (target) {
+        if (allowedTypes.indexOf(target.type) === -1) {
+          errors.push(`Unsupported file type: ${target.name}`);
+          readNext();
+          return;
+        }
+        fileName = target.name;
+        reader.readAsBinaryString(target);
+      } else {
+        this.setState({errors: [...this.state.errors, ...errors]});
+      }
+    };
 
-		reader.onload = () => {
+    reader.onload = () => {
       this.processJson(reader.result, fileName);
-		};
+    };
 
-		reader.onloadend = function() {
-			readNext();
-		};
+    reader.onloadend = function() {
+      readNext();
+    };
 
-		reader.onerror = function() {
-			this.setState({errors: [...this.state.errors, reader.error]});
-		};
+    reader.onerror = function() {
+      this.setState({errors: [...this.state.errors, reader.error]});
+    };
 
-		readNext();
-	}
+    readNext();
+  }
 
-	onSelectFiles(event) {
-		this.readFiles(event.target.files);
-		this.clearOutput();
-	}
+  onSelectFiles(event) {
+    this.readFiles(event.target.files);
+    this.clearOutput();
+  }
 
-	onDragOverListener(event) {
+  onDragOverListener(event) {
     event.preventDefault();
-		if (event.dataTransfer.types.indexOf('Files') !== -1) {
-			this.setDraggableClass(event.currentTarget, true);
-		}
+    if (event.dataTransfer.types.indexOf('Files') !== -1) {
+      this.setDraggableClass(event.currentTarget, true);
+    }
   }
 
-	onDropListener(event) {
-    event.preventDefault();
-    this.readFiles(event.dataTransfer.files);
+  onDropListener(event) {
+  event.preventDefault();
+  this.readFiles(event.dataTransfer.files);
 
-		this.setDraggableClass(event.currentTarget, false);
+    this.setDraggableClass(event.currentTarget, false);
   }
 
-	onDragLeaveListener(event) {
-		this.setDraggableClass(event.currentTarget, false);
+  onDragLeaveListener(event) {
+    this.setDraggableClass(event.currentTarget, false);
   }
 
-	onDragEnterListener(event) {
-		this.clearOutput();
-	}
+  onDragEnterListener(event) {
+    this.clearOutput();
+  }
 
-	clearOutput() {
-		this.setState({result: [], errors: []});
-	}
+  clearOutput() {
+    this.setState({result: [], errors: []});
+  }
 
   render() {
     return (
-    	<div className={styles.wrapper}>
-	      <form
-	        onDragOver={this.onDragOverListener}
-	        onDrop={this.onDropListener}
-	        onDragLeave={this.onDragLeaveListener}
-	        onDragEnter={this.onDragEnterListener}
-	        className={styles.form}
-	        encType="multipart/form-data">
-		      <label
-			      className={styles.label}
-			      htmlFor="file">
-			      Drag files here or click to upload (.json)
-		        <input
-		          className={styles.input}
-		          type="file"
-		          multiple
-		          onChange={this.onSelectFiles}/>
-		      </label>
-		      <div className={styles['wrapper-result']}>
-			      {this.state.result.map((result, key) => <span key={key} className={styles.result}>{result}</span>)}
-			      {this.state.errors.map((error, key) => <span key={key} className={styles.error}>{error}</span>)}
-		      </div>
-	      </form>
+      <div className={styles.wrapper}>
+      <form
+        onDragOver={this.onDragOverListener}
+        onDrop={this.onDropListener}
+        onDragLeave={this.onDragLeaveListener}
+        onDragEnter={this.onDragEnterListener}
+        className={styles.form}
+        encType="multipart/form-data">
+        <label
+          className={styles.label}
+          htmlFor="file">
+          Drag files here or click to upload (.json)
+        <input
+          className={styles.input}
+          type="file"
+          multiple
+          onChange={this.onSelectFiles}/>
+        </label>
+        <div className={styles['wrapper-result']}>
+          {this.state.result.map((result, key) => <span key={key} className={styles.result}>{result}</span>)}
+          {this.state.errors.map((error, key) => <span key={key} className={styles.error}>{error}</span>)}
+        </div>
+      </form>
 
-	    </div>
+      </div>
     )
   }
 }
